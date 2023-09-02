62d86fc1bbcab0f959ffea4f32825e6765a2c629
Joost Ruis <joost.ruis@mocaccino.org>
2023-09-02 23:21:13 +0200

    bump

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cp luet_pm_gui.png 	/usr/share/icons/hicolor/256x256/apps/luet_pm_gui.png
 cp luet_pm_gui.desktop 	/usr/share/applications/luet_pm_gui.desktop
 cp luet_pm_gui.py  luet_pm_ui.sh /usr/bin
-chmod +x /usr/bin/luet_pm_gui.py /usr/bin/luet_pm_ui.sh
+chmod +x /usr/bin/luet_pm_gui.py /usr/bin/luet_pm_gui.sh
