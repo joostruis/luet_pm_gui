eb52f7b4b7779abaf6eec5c1bc1d1a9bc31a53d9
Joost Ruis <joost.ruis@mocaccino.org>
2025-08-11 19:13:50 +0200

    install base icon in /usr/share/pixmaps

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /usr/share/icons/hicolor/256x256/apps && cp luet_pm_gui.png /usr/share/icons/hicolor/256x256/apps/luet_pm_gui.png
+mkdir -p /usr/share/pixmaps && cp luet_pm_gui.png /usr/share/pixmaps/luet_pm_gui.png
 mkdir -p /usr/share/applications/ && cp luet_pm_gui.desktop /usr/share/applications/luet_pm_gui.desktop
 cp luet_pm_gui.py  luet_pm_gui.sh /usr/bin
 cp org.example.luet_pm_gui.policy /usr/share/polkit-1/actions
