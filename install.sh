1c427edb156e114b4437729f2d51dcbf1ce669c3
Joost Ruis <joost.ruis@mocaccino.org>
2025-09-16 21:44:09 +0200

    rename nl -> nl_NL and update install script

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ cp luet_pm_gui.py  luet_pm_gui.sh /usr/bin
 cp org.mocaccino.luet.pm.gui.policy /usr/share/polkit-1/actions
 chmod +x /usr/bin/luet_pm_gui.py /usr/bin/luet_pm_gui.sh
 mkdir -p /etc/polkit-1/rules.d/ && cp 99-luet.rules /etc/polkit-1/rules.d/99-luet.rules && chown root:root /etc/polkit-1/rules.d/99-luet.rules && chmod 644 /etc/polkit-1/rules.d/99-luet.rules
+
+# Install translations
+for lang in $(ls locale); do
+    if [ -f locale/$lang/LC_MESSAGES/luet_pm_gui.mo ]; then
+        mkdir -p /usr/share/locale/$lang/LC_MESSAGES
+        cp locale/$lang/LC_MESSAGES/luet_pm_gui.mo /usr/share/locale/$lang/LC_MESSAGES/
+    fi
+done
