ee682cc5ca6a84e94c69a6e088f4900c56b8e94c
joost_op <joost.ruis@mocaccino.org>
2026-03-19 18:23:14 +0100

    Refactor install.sh to use PACKAGE_DIR for paths
    Updated install script to use PACKAGE_DIR for installation paths.

--- install.sh
+++ install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 set -e
 
+PACKAGE_DIR="/package"
+
 # Install icon
-install -Dm644 vajo.png /usr/share/pixmaps/vajo.png
+install -Dm644 vajo.png $PACKAGE_DIR/usr/share/pixmaps/vajo.png
 
 # Install desktop entry
-install -Dm644 vajo.desktop /usr/share/applications/vajo.desktop
+install -Dm644 vajo.desktop $PACKAGE_DIR/usr/share/applications/vajo.desktop
 
 # Install executables
-install -Dm755 luet_pm_tui.py /usr/bin/luet_pm_tui.py
-install -Dm755 luet_pm_gui.py /usr/bin/luet_pm_gui.py
-install -Dm755 vajo.sh /usr/bin/vajo.sh
+install -Dm755 luet_pm_tui.py $PACKAGE_DIR/usr/bin/luet_pm_tui.py
+install -Dm755 luet_pm_gui.py $PACKAGE_DIR/usr/bin/luet_pm_gui.py
+install -Dm755 vajo.sh $PACKAGE_DIR/usr/bin/vajo.sh
 
 # Create convenient symlinks for easier launching
-ln -sf /usr/bin/luet_pm_gui.py /usr/bin/vajo-gui
-ln -sf /usr/bin/luet_pm_tui.py /usr/bin/vajo-tui
+ln -sf $PACKAGE_DIR/usr/bin/luet_pm_gui.py $PACKAGE_DIR/usr/bin/vajo-gui
+ln -sf $PACKAGE_DIR/usr/bin/luet_pm_tui.py $PACKAGE_DIR/usr/bin/vajo-tui
 
 # Install polkit policy + rules
-install -Dm644 org.mocaccino.vajo.policy /usr/share/polkit-1/actions/org.mocaccino.vajo.policy
-install -Dm644 99-luet.rules /etc/polkit-1/rules.d/99-luet.rules
-chown root:root /etc/polkit-1/rules.d/99-luet.rules
+install -Dm644 org.mocaccino.vajo.policy $PACKAGE_DIR/usr/share/polkit-1/actions/org.mocaccino.vajo.policy
+install -Dm644 99-luet.rules $PACKAGE_DIR/etc/polkit-1/rules.d/99-luet.rules
+chown root:root $PACKAGE_DIR/etc/polkit-1/rules.d/99-luet.rules
 
 # Install translations
 for lang in $(ls locale 2>/dev/null); do
     if [ -f "locale/$lang/LC_MESSAGES/luet_pm_ui.mo" ]; then
         install -Dm644 "locale/$lang/LC_MESSAGES/luet_pm_ui.mo" \
-            "/usr/share/locale/$lang/LC_MESSAGES/luet_pm_ui.mo"
+            "$PACKAGE_DIR/usr/share/locale/$lang/LC_MESSAGES/luet_pm_ui.mo"
     fi
 done
 
 # Install Python core module to a version-independent location
-SHARED_DIR="/usr/share/vajo"
+SHARED_DIR="$PACKAGE_DIR/usr/share/vajo"
 install -d "$SHARED_DIR"
 install -m644 luet_pm_core.py "$SHARED_DIR/luet_pm_core.py"
